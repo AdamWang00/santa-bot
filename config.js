be0a9b50fe3311ef4d423a9deb8600608a585fce
Adam Wang <adam_wang1@brown.edu>
2020-06-21 17:25:00 -0400

    update

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 var fs = require('fs');
 
-exports.TOKEN = 'NTE2NDE0MzI2MzkxNzAxNTI0.XcN9Mg._sWWewITy5vGpYLZvqoXhEwaVBg';
-//'NjMyNzUzMTc0OTM5MTcyODk1.XaLHhQ.kIqCZY9FgFAjyuFGOoKhyOEnpiQ';
+exports.TOKEN = process.env.DISCORD_BOT_TOKEN;
 
 exports.PREFIX = '=';
 
-exports.GOOGLE_API_KEY = 'AIzaSyC_JCoSoTm8r0q7_3VXxwalzpj9IgXtYC0';
+exports.GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
 exports.HANGMAN_LIST = fs.readFileSync('hangmanWords.txt').toString().split('\r\n');
 
